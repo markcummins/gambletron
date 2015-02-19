09710e8434eb476fd99be764760527cdce29efdb
mark-cummins <mark@bluechief.ie>
2015-02-19 00:51:29 +0000

    first commit

---
+++ application/views/v_footer.php
@@ -0,0 +1,28 @@
+<br/>
+<hr/>
+<footer>
+	<p>&copy; ScratchyEar LTD 2015</p>
+</footer>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+<script src="<?=base_url('assets/js/vendor/bootstrap.min.js');?>"></script>
+<script src="<?=base_url('assets/js/vendor/jquery-ui-1.11.3/jquery-ui.js');?>"></script>
+<script src="<?=base_url('assets/js/vendor/moment.js');?>"></script>
+<script src="<?=base_url('assets/js/vendor/Chart.Core.js');?>"></script>
+<script src="<?=base_url('assets/js/vendor/Chart.Doughnut.js');?>"></script>
+<script src="<?=base_url('assets/js/vendor/jsonsql.js');?>"></script>
+<script src="<?=base_url('assets/js/main.js');?>"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+															function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+						   e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+						   e.src='//www.google-analytics.com/analytics.js';
+						   r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+
+</body>
+</html>
